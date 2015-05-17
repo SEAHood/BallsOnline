043f1046811711ca107e3648c9f31c781e304c79
SEAHood <me@samhood.net>
2015-05-17 22:03:23 +0100

    Committing initial files
    Currently working with chat, networking capabilities, FPS counter, basic
    movement.  Needs tidied into OO

---
+++ index.js
@@ -0,0 +1,35 @@
+//var app = require('express')();
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+//app.use('/js', express.static(__dirname + '/node_modules/'));
+
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/img'));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    var address = socket.request.connection.remoteAddress;
+	console.log("Connection from: " + address);	
+	
+	socket.on('movement', function(player) {
+		io.emit('movement', player);
+		var seconds = new Date().getTime() / 1000;
+		console.log('[' + seconds + ']' + player.guid);
+	});
+	
+	
+	socket.on('chat message', function(msg, user){
+		console.log("Message received from " + user + " - Body: " + msg);
+		io.emit('chat message', msg, user);
+    });
+});
+
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
